c794e21e252f6076526fdc66a25f01bb6a00e848
alt--er <ogasawara.t.ae@m.titech.ac.jp>
2025-02-19 01:55:45 +0900

    add:DB構成

--- initsql/initsql.sql
+++ initsql/initsql.sql
@@ -5,8 +5,19 @@ CREATE USER 'anka'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON anka.* TO 'anka'@'%';
 FLUSH PRIVILEGES;
 -- 以下traQer関連
-CREATE TABLE IF NOT EXISTS `ankaDB` (
-  `ankaId` char(36) NOT NULL,
-  `ankaInvokeMessageNum` int(11) NOT NULL,
-  PRIMARY KEY (`ankaId`)
+
+CREATE TABLE IF NOT EXISTS `ankaChannel` (
+  `Id` char(36) NOT NULL,
+  `channelMessageCount` int(11) NOT NULL,
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `ankaDB` (
+  `Id` char(36) NOT NULL,
+  `channelId` char(36) NOT NULL,
+  `ankaInvokeMessageNum` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  FOREIGN KEY (`channelId`) REFERENCES `ankaChannel`(`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
